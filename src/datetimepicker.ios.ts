a49fb68a02c9b8e4bfa0bb3dd61a4253b4f0782d
darkyelox <dfmv124@gmail.com>
2018-09-27 10:57:52 -0500

    Some adjusts for start IOS dev

--- src/datetimepicker.ios.ts
+++ src/datetimepicker.ios.ts
@@ -1,4 +1,4 @@
-import { IDateTimePicker, Time } from './datetimepicker.common';
+import { IDateTimePicker, Time, IDatePicker, ITimePicker } from './datetimepicker.common';
 import { Color } from 'tns-core-modules/color';
 
 export class DatetimePicker implements IDateTimePicker {
@@ -39,4 +39,63 @@ export class DatetimePicker implements IDateTimePicker {
 
     }
     
+}
+
+export class DatePicker implements IDatePicker {
+
+    setColor(color: Color) {
+        
+    }
+
+    showAsPicker(asPicker: boolean) {
+        
+    }
+
+    autoDismiss(dismiss: boolean) {
+        
+    }
+
+    setThemeDark(isThemeDark: boolean) {
+        
+    }
+
+    setMinDate(minDate: Date) {
+        
+    }    
+    
+    setMaxDate(maxData: Date) {
+        
+    }
+
+    showYearPickerFirst(showYearFirst: boolean) {
+        
+    }
+    
+}
+
+export class TimePicker implements ITimePicker {
+    setColor(color: Color) {
+        
+    }
+
+    showAsPicker(asPicker: boolean) {
+        
+    }
+
+    autoDismiss(dismiss: boolean) {
+        
+    }
+
+    setThemeDark(isThemeDark: boolean) {
+        
+    }
+
+    setMinTime(minTime: Time) {
+        
+    }
+    
+    setMaxTime(maxTime: Time) {
+        
+    }
+
 }
